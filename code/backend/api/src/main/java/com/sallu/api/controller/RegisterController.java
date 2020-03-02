a975765bdcac8784ce42dcc200cc7f7e22863135
putschris <gui89cris1157@gmail.com>
2020-03-02 05:20:34 -0300

    Login e Logout alterações

--- code/backend/api/src/main/java/com/sallu/api/controller/RegisterController.java
+++ code/backend/api/src/main/java/com/sallu/api/controller/RegisterController.java
@@ -8,14 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
-public class UserController
+@RequestMapping("/register")
+public class RegisterController
 {
     private User userActual;
 
-    @PostMapping("/register/{nome}/{email}/{password}/{confirmPassword}")
+    @PostMapping("/{nome}/{email}/{password}/{confirmPassword}")
     public void registerUser(@PathVariable("nome")String nome, @PathVariable("email") String email, @PathVariable("password")String password, @PathVariable("confirmPassword")String confirmPassword)
     {
+        if(nome.equals(null) || email.equals(null) || password.equals(null) || confirmPassword.equals(null))
+        {
+            System.out.println("Preencha todos os campos");
+        }
+        else if(password != confirmPassword)
+        {
+            System.out.println("As senhas devem ser iguais");
+        }
+        else{
+            userActual = new User(nome, email, password, confirmPassword);
+            System.out.println("Usuário cadastrado, bem vindo!");
+        }
 
     }
 
--- code/backend/api/src/main/java/com/sallu/api/domain/User.java
+++ code/backend/api/src/main/java/com/sallu/api/domain/User.java
@@ -1,4 +1,38 @@
 package com.sallu.api.domain;
 
-public class User {
+
+public class User
+{
+    private String name;
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+    public User(String name, String email, String password, String confirmPassword)
+    {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public String getConfirmPassword()
+    {
+        return confirmPassword;
+    }
 }
