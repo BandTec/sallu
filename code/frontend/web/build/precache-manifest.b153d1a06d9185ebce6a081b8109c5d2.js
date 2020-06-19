90d5b3df9d3b4d5ea4d1850b3dffa5d3b8a8bf54
1506kidrauhl <huayhuamarca19@gmail.com>
2020-06-19 17:10:14 -0300

    deploy

---
+++ code/frontend/web/build/precache-manifest.b153d1a06d9185ebce6a081b8109c5d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b96098282bc016b19cd82a3ceb3a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c513d23e5646bceb90ef",
+    "url": "/static/css/main.f06c7cec.chunk.css"
+  },
+  {
+    "revision": "93869b58a8873de5853e",
+    "url": "/static/js/2.08ed3d56.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.08ed3d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c513d23e5646bceb90ef",
+    "url": "/static/js/main.2feffa5d.chunk.js"
+  },
+  {
+    "revision": "602f1dfffe587fe210d1",
+    "url": "/static/js/runtime-main.9cbdf16c.js"
+  },
+  {
+    "revision": "8e6468ee767853901c8fb3dba5b9801f",
+    "url": "/static/media/avatar.8e6468ee.png"
+  },
+  {
+    "revision": "54bdd972346319a3293c3b047a4d26f8",
+    "url": "/static/media/config.54bdd972.png"
+  },
+  {
+    "revision": "367a4d8dbea4706f77b298a4a1a1531d",
+    "url": "/static/media/doctors.367a4d8d.svg"
+  },
+  {
+    "revision": "18fc36d23625abf56d203903b18fd54c",
+    "url": "/static/media/historico.18fc36d2.png"
+  },
+  {
+    "revision": "f04609cbe62d46f81927fcf572aa9112",
+    "url": "/static/media/logo_salut_normal.f04609cb.svg"
+  },
+  {
+    "revision": "fe5454b649c1a5ee1b2966e7e86fa3b8",
+    "url": "/static/media/medicine.fe5454b6.png"
+  },
+  {
+    "revision": "60841ae8b5a6217de9cff1b713a7cdd5",
+    "url": "/static/media/pacient.60841ae8.jpg"
+  },
+  {
+    "revision": "47193ab7c9ccc24a4266d8d49269eb99",
+    "url": "/static/media/walk-dog.47193ab7.svg"
+  }
+]);
