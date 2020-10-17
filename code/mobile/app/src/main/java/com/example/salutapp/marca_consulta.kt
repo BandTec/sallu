e007ad9e68805058f2db846d13487efc1a813bda
unknown <giovanna.ab.souza10@gmail.com>
2020-10-16 22:35:27 -0300

    Tela Consulta

---
+++ code/mobile/app/src/main/java/com/example/salutapp/marca_consulta.kt
@@ -0,0 +1,11 @@
+package com.example.salutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class marca_consulta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marca_consulta)
+    }
+}
