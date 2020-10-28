e3125ee9ce4f1e108c8f2ce4442f060b2dfe1702
httpchris <guilherme.costa@bandtec.com.br>
2020-10-28 15:43:52 -0300

    alterações necessárias

--- code/mobile/app/src/main/java/com/example/salutapp/MenuActivity.kt
+++ code/mobile/app/src/main/java/com/example/salutapp/MenuActivity.kt
@@ -1,11 +1,53 @@
 package com.example.salutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var iv_power: ImageView = findViewById(R.id.iv_power);
+        var iv_settings:ImageView = findViewById(R.id.iv_settings)
+        var cd_emergence: androidx.cardview.widget.CardView = findViewById(R.id.cd_emergence);
+        var cd_historico: androidx.cardview.widget.CardView = findViewById(R.id.cd_historico);
+
+        iv_power.setOnClickListener {
+            turnOff()
+        }
+
+        cd_emergence.setOnClickListener {
+            enterEmergence()
+        }
+
+        cd_historico.setOnClickListener {
+            enterHistorico()
+        }
+
+        iv_settings.setOnClickListener {
+            enterSettings()
+        }
+    }
+
+    private fun turnOff() {
+        finishAffinity()
+    }
+
+    private fun enterEmergence() {
+        val intent = Intent(this, InteractiveDollActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun enterHistorico() {
+        val intent = Intent(this, HistoricoMedico::class.java)
+        startActivity(intent)
+    }
+
+    private fun enterSettings() {
+        val intent = Intent(this, ConfiguracaoActivity::class.java)
+        startActivity(intent)
     }
 }
--- code/mobile/app/src/main/java/com/example/salutapp/RegisterActivity.kt
+++ code/mobile/app/src/main/java/com/example/salutapp/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package com.example.salutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.example.salutapp.api.Registro
 import com.example.salutapp.api.RetrofitConfig
 import kotlinx.android.synthetic.main.activity_register.*
@@ -11,7 +14,19 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        var btnRegister: Button = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener {
+            openMenu()
+        }
     }
+
+    private fun openMenu() {
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
+    }
+
 
    /* fun cadastro(componente:View){
         Registro registro = new Registro()
