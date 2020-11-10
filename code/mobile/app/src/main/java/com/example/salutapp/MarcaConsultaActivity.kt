28321bf9ec33af4c6625f4cddc59bb046116aed7
httpchris <guilherme.costa@bandtec.com.br>
2020-11-10 20:55:17 -0300

    alterações

--- code/mobile/app/src/main/java/com/example/salutapp/MarcaConsultaActivity.kt
+++ code/mobile/app/src/main/java/com/example/salutapp/MarcaConsultaActivity.kt
@@ -1,7 +1,9 @@
 package com.example.salutapp
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MarcaConsultaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- code/mobile/app/src/main/java/com/example/salutapp/MenuActivity.kt
+++ code/mobile/app/src/main/java/com/example/salutapp/MenuActivity.kt
@@ -4,50 +4,71 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_marca_consulta.*
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        var iv_power: ImageView = findViewById(R.id.iv_power);
-        var iv_settings:ImageView = findViewById(R.id.iv_settings)
         var cd_emergence: androidx.cardview.widget.CardView = findViewById(R.id.cd_emergence);
+        var cd_ficha_medica: androidx.cardview.widget.CardView = findViewById(R.id.cd_ficha_medica_menu);
+        var cd_marca_consulta: androidx.cardview.widget.CardView = findViewById(R.id.cd_marca_consulta);
         var cd_historico: androidx.cardview.widget.CardView = findViewById(R.id.cd_historico);
 
-        iv_power.setOnClickListener {
-            turnOff()
-        }
+        var iv_power: ImageView = findViewById(R.id.iv_power);
+        var iv_settings: ImageView = findViewById(R.id.iv_settings);
 
         cd_emergence.setOnClickListener {
-            enterEmergence()
+            nextEmergence();
+        }
+
+        cd_ficha_medica.setOnClickListener {
+            nextMarca();
+        }
+
+        cd_marca_consulta.setOnClickListener {
+            nextFicha();
         }
 
         cd_historico.setOnClickListener {
-            enterHistorico()
+            nextHistorico();
+        }
+
+        iv_power.setOnClickListener {
+            turnOff();
         }
 
         iv_settings.setOnClickListener {
-            enterSettings()
+            nextSettings();
         }
+    }
+    private fun nextEmergence() {
+        val intent = Intent(this,InteractiveDollActivity::class.java)
+        startActivity(intent);
+    }
+
+    private fun nextFicha() {
+        val intent = Intent(this, FichaMedica::class.java)
+        startActivity(intent);
+    }
+
+    private fun nextMarca() {
+        val intent = Intent(this, MarcaConsultaActivity::class.java)
+        startActivity(intent);
+    }
+
+    private fun nextHistorico() {
+        val intent = Intent(this, HistoricoMedico::class.java)
+        startActivity(intent);
     }
 
     private fun turnOff() {
-        finishAffinity()
+        finishAffinity();
     }
 
-    private fun enterEmergence() {
-        val intent = Intent(this, InteractiveDollActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun enterHistorico() {
-        val intent = Intent(this, HistoricoMedico::class.java)
-        startActivity(intent)
-    }
-
-    private fun enterSettings() {
+    private fun nextSettings() {
         val intent = Intent(this, ConfiguracaoActivity::class.java)
-        startActivity(intent)
+        startActivity(intent);
     }
 }
