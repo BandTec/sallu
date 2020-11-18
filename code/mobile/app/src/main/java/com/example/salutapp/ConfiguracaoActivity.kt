f61d19cbe8ba68aecc0e5c8391b62fa626ad6ef7
1506kidrauhl <huayhuamarca19@gmail.com>
2020-11-17 23:49:53 -0300

    Endpoint alterar dados funcionando

--- code/mobile/app/src/main/java/com/example/salutapp/ConfiguracaoActivity.kt
+++ code/mobile/app/src/main/java/com/example/salutapp/ConfiguracaoActivity.kt
@@ -1,11 +1,15 @@
 package com.example.salutapp
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.core.text.set
 import com.example.salutapp.api.RetrofitConfig
+import com.example.salutapp.api.dto.UsuarioDto
 import com.example.salutapp.api.model.Usuario
 import kotlinx.android.synthetic.main.activity_configuracao.*
 import kotlinx.android.synthetic.main.activity_menu.*
@@ -14,6 +18,7 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class ConfiguracaoActivity : AppCompatActivity() {
+    var preferencias: SharedPreferences? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracao)
@@ -46,5 +51,33 @@ class ConfiguracaoActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    fun alterarDados(v: View){
+        if (swAlteracaoFinalizada.isChecked) {
+            val api = RetrofitConfig().requestUsuario()
+            val usuario= UsuarioDto(
+                    et_nome.text.toString(),
+                    et_gmail.text.toString(),
+                    et_telefone.text.toString(),
+                    false,
+                    et_nascimento.text.toString(),
+                    et_genero.text.toString()
+            )
+            val callUsuario = api.putUsuarioDados(usuario)
+
+            callUsuario.enqueue(object: Callback<Void> {
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    Toast.makeText(applicationContext, "Deu ruim $t", Toast.LENGTH_SHORT).show()
+                }
+                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                    if(response.code() == 200) {
+                        Toast.makeText(this@ConfiguracaoActivity,getString(R.string.msg_alterar_sucesso_config), Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this@ConfiguracaoActivity, getString(R.string.msg_alterar_erro_config), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+        }
     }
 }
